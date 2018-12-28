626f59b6fcced9e50ebf5a2283c13707d05b44e1
Siv Elin Ødegård <sivelin.odegard@students.fhnw.ch>
2018-12-28 17:16:48 +0100

    slider visible, but  without functions

--- js/init.js
+++ js/init.js
@@ -5,4 +5,19 @@
     $('.parallax').parallax();
 
   }); // end of document ready
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+  $('.slider').slider('start');
+  $('.slider').slider('methodName', paramName);
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options); //Die variabel wird nie aufgerufen, wie muss ich sie bei jQuery aufrufen?
+});
+
+var instance = M.Slider.getInstance(elem);
+instance.start();
+instance.next();
